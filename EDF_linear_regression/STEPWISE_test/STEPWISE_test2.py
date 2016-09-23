1c0e47f831f42b25aee37c79cfa9ccf2da0ba7a8
Denis Barbier <barbier@imacs.polytechnique.fr>
2016-09-23 12:55:25 +0200

    Describe API changes as discussed with Sofiane

--- EDF_linear_regression/STEPWISE_test/STEPWISE_test2.py
+++ EDF_linear_regression/STEPWISE_test/STEPWISE_test2.py
@@ -11,23 +11,23 @@ X = Sample[:, 1:4]
 Y = Sample[:, 0]
 
 # Build a model BIO~SAL+pH+K+Na+Zn
-factory = ot.LinearModelStepwiseFactory(X, Y)
-factory.add(factory.getInteractions(X.getDescription(), 1))
+factory = ot.LinearModelStepwiseFactory(X.getDescription())
+factory.add(factory.getInteractions(1))
 
 i_0 = factory.getIndices(["1"])
 i_min = i_0
 
 ## Forward
-lm_forward_AIC_result = factory.buildForward(i_min, 2)
-lm_forward_BIC_result = factory.buildForward(i_min, log(45))
+lm_forward_AIC_result = factory.build(X, Y, 1, i_min, [], 2)
+lm_forward_BIC_result = factory.build(X, Y, 1, i_min, [], log(45))
 
 ## Backward
-lm_backward_AIC_result = factory.buildBackward(i_min, 2)
-lm_backward_BIC_result = factory.buildBackward(i_min, log(45))
+lm_backward_AIC_result = factory.build(X, Y, 2, i_min, [], 2)
+lm_backward_BIC_result = factory.build(X, Y, 2, i_min, [], log(45))
 
 ## Both
-lm_both_AIC_result = factory.buildBoth(i_min, i_0, 2)
-lm_both_BIC_result = factory.buildBoth(i_min, i_0, log(45))
+lm_both_AIC_result = factory.build(X, Y, 3, i_min, i_0, 2)
+lm_both_BIC_result = factory.build(X, Y, 3, i_min, i_0, log(45))
 
 
 lm_forward_AIC_result.printANOVAtable()
