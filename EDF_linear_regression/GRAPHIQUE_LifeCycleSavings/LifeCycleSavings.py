8dfa42ed98f40b39b4f40c8f6e12a4a4dc402568
Denis Barbier <barbier@imacs.polytechnique.fr>
2016-10-20 18:38:35 +0200

    Update LifeCycleSavings.py

--- EDF_linear_regression/GRAPHIQUE_LifeCycleSavings/LifeCycleSavings.py
+++ EDF_linear_regression/GRAPHIQUE_LifeCycleSavings/LifeCycleSavings.py
@@ -24,33 +24,34 @@ inputSample = Sample[:,1:5]
 
 algo1 = ot.LinearModelAlgorithm(inputSample, outputSample)
 result1 = algo1.getResult()
+analysis1 = ot.LinearModelAnalysis(algo1.getResult())
 
-# for plot in ["drawResidualsVsFitted", "drawScaleLocation", "drawQQplot", "drawCookDistance", "drawResidualsVsLeverages", "drawCookVsLeverages"]:
-#     graph = getattr(result1, plot)()
-#     View(graph)
+for plot in ["drawResidualsVsFitted", "drawScaleLocation", "drawQQplot", "drawCookDistance", "drawResidualsVsLeverages", "drawCookVsLeverages"]:
+    graph = getattr(analysis1, plot)()
+    graph.draw("model1-"+plot, 640, 480)
 
 # plot of residuals versus fitted values
-graph = result1.drawResidualsVsFitted()
+graph = analysis1.drawResidualsVsFitted()
 View(graph)
 
 # scale-location plot of sqrt(|residuals|) versus fitted values
-graph = result1.drawScaleLocation()
+graph = analysis1.drawScaleLocation()
 View(graph)
 
 # Normal quantiles-quantiles plot of standardized residuals
-graph = result1.drawQQplot()
+graph = analysis1.drawQQplot()
 View(graph)
 
 # plot of Cook's distances versus row labels
-graph = result1.drawCookDistance()
+graph = analysis1.drawCookDistance()
 View(graph)
 
 # plot of residuals versus leverages that adds bands corresponding to Cook's distances of 0.5 and 1
-graph = result1.drawResidualsVsLeverages()
+graph = analysis1.drawResidualsVsLeverages()
 View(graph)
 
 # plot of Cook's distances versus leverage/(1-leverage)
-graph = result1.drawCookVsLeverages()
+graph = analysis1.drawCookVsLeverages()
 View(graph)
 
 # model2
@@ -61,28 +62,33 @@ inputSample.stack(dpi)
 
 algo2 = ot.LinearModelAlgorithm(inputSample, outputSample)
 result2 = algo2.getResult()
+analysis2 = ot.LinearModelAnalysis(algo2.getResult())
+
+for plot in ["drawResidualsVsFitted", "drawScaleLocation", "drawQQplot", "drawCookDistance", "drawResidualsVsLeverages", "drawCookVsLeverages"]:
+    graph = getattr(analysis2, plot)()
+    graph.draw("model2-"+plot, 640, 480)
 
 # plot of residuals versus fitted values
-graph = result2.drawResidualsVsFitted()
+graph = analysis2.drawResidualsVsFitted()
 View(graph)
 
 # scale-location plot of sqrt(|residuals|) versus fitted values
-graph = result2.drawScaleLocation()
+graph = analysis2.drawScaleLocation()
 View(graph)
 
 # Normal quantiles-quantiles plot of standardized residuals
-graph = result2.drawQQplot()
+graph = analysis2.drawQQplot()
 View(graph)
 
 # plot of Cook's distances versus row labels
-graph = result2.drawCookDistance()
+graph = analysis2.drawCookDistance()
 View(graph)
 
 # plot of residuals versus leverages that adds bands corresponding to Cook's distances of 0.5 and 1
-graph = result2.drawResidualsVsLeverages()
+graph = analysis2.drawResidualsVsLeverages()
 View(graph)
 
 # plot of Cook's distances versus leverage/(1-leverage)
-graph = result2.drawCookVsLeverages()
+graph = analysis2.drawCookVsLeverages()
 View(graph)
 
